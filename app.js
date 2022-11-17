df32469c120c2993a7fff1a5becdbe7c0a936165
biethereos <nguyenthanhhung@gmail.com>
2022-11-17 11:59:15 +0700

    Handling Different Routes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // alows the request to continue to next middleware in line
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>The Add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware!");
   res.send("<h1>Hello from Epress!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);

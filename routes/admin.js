c4e53de124c3d0850d0729c07e8ab9db269111a7
biethereos <32012536+biethereos@users.noreply.github.com>
2022-11-17 22:55:39 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get("/add-product", (req, res, next) => {
 });
 
 router.post("/product", (req, res, next) => {
-  console.log(res.body);
+  console.log(req.body);
   res.redirect("/");
 });
 

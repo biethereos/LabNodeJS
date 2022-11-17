9fa6d317c55e2f3a832b093cb4dadc039459a9ad
biethereos <nguyenthanhhung@gmail.com>
2022-11-17 22:29:58 +0700

    Using Express Router

--- app.js
+++ app.js
@@ -2,22 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='tittle'/><button type='submit'>Add Product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Epress!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);

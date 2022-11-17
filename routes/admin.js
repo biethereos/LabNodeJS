fca960870f9e8042fc2cd89912d56af716e1c2e3
biethereos <nguyenthanhhung@gmail.com>
2022-11-17 23:06:33 +0700

    Using Express Router

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form"
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
   );
 });
 
 router.post("/product", (req, res, next) => {
-  console.log(res.body);
+  console.log(req.body);
   res.redirect("/");
 });
 

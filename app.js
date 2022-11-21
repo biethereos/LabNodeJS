d4660a2cfed0a7a097212656bf03f06ea9b9e2e2
biethereos <nguyenthanhhung@gmail.com>
2022-11-21 23:06:58 +0700

    Installing & Implementing Pug

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 

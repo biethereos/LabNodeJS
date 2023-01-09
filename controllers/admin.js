6ff0b69bc2e744de6049e52cdc25e4509fe44a46
biethereos <nguyenthanhhung@gmail.com>
2023-01-10 00:48:43 +0700

    Making the "Edit" & "Delete" Buttons Work Again

--- controllers/admin.js
+++ controllers/admin.js
@@ -76,9 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
-    // Product.findAll()
+  Product.fetchAll()
     .then((products) => {
       res.render('admin/products', {
         prods: products,

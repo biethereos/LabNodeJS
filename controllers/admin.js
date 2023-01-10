08d01de79650f5968059ac48183080dc433308c1
biethereos <nguyenthanhhung@gmail.com>
2023-01-10 14:53:45 +0700

    One Note About Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,4 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -57,13 +55,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    new ObjectId(prodId)
-  );
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
   product
     .save()
     .then((result) => {

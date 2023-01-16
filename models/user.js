8df5bbe6e7fa228ddde09b96b719f67a91a7fd51
biethereos <nguyenthanhhung@gmail.com>
2023-01-16 23:28:15 +0700

    Adding Relational Order Data

--- models/user.js
+++ models/user.js
@@ -70,15 +70,28 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.username,
+          },
+        };
+        return db.collection('orders').insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
           .collection('users')
           .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: { items: [] } } });
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    // return db.collection('orders')
   }
 
   static findById(userId) {

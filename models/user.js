d2f9e5908eec74c2969dfcea06c6568a36f0e546
biethereos <nguyenthanhhung@gmail.com>
2023-01-16 22:42:20 +0700

    Adding an Order

--- models/user.js
+++ models/user.js
@@ -68,6 +68,19 @@ class User {
       .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: { items: updatedCartItems } } });
   }
 
+  addOrder() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: { items: [] } } });
+      });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db

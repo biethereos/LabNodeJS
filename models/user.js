677fec3219d557a084ea5c80c77e0c2ba7d5e17b
biethereos <nguyenthanhhung@gmail.com>
2023-02-06 22:09:25 +0700

    Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -20,6 +20,14 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 userSchema.methods.addToCart = function (product) {
   const cartProductIndex = this.cart.items.findIndex((cp) => {
     return cp.productId.toString() === product._id.toString();
